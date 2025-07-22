bae9dac9495e50c7a580939dfceb8ea59c5731d7
Sheriff <mssheriff114@gmail.com>
2025-07-22 12:33:33 +0530

    first commit

---
+++ app/(tabs)/AI.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AIScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>🤖 AI Screen</Text>
+    </View>
+  );
+}
